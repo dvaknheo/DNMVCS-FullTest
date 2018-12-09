429d82e752747490775c0ef5f5832c7cf279eb91
dvakheo <dvaknheo@gmail.com>
2018-12-09 18:38:49 +0800

    save

--- public/u/app/Controller/Main.php
+++ public/u/app/Controller/Main.php
@@ -2,7 +2,9 @@
 namespace MY\Controller;
 use DNMVCS\DNMVCS as DN;
 use DNMVCS\SuperGlobal as SG;
-use MY\Service as S;
+use DNMVCS\Pager;
+use DNMVCS\DNException;
+
 use MY\Service\SessionService;
 use MY\Service\ArticleService;
 use MY\Service\UserService;
@@ -11,19 +13,6 @@ class DNController
 {
 	public function __construct()
 	{
-		DN::Import('Pager');
-		/*
-		DN::G()->assignExceptionHandler('DNMVCS\DNException',function($ex){
-
-			var_dump($ex->getMessage());
-		});
-			*/
-	}
-	public function i()
-	{
-		$t=SG::G()->_SERVER;
-		ksort($t);
-		var_export($t);
 	}
 	public function index()
 	{
@@ -54,12 +43,12 @@ class DNController
 		$page=($page>1)?:1;
 		$page_size=10;
 		
-		$article=S\ArticleService::G()->getArticleFullInfo($id,$page,$page_size);
+		$article=ArticleService::G()->getArticleFullInfo($id,$page,$page_size);
 		if(!$article){
 			DN::G()->onShow404();return;
 		}
 		DN::RecordsetH($article['comments'],['content','username']);
-		$html_pager=(new \Pager($article['comments_total'],$page_size))->get_page_output();
+		$html_pager=Pager::Render($article['comments_total']);
 		$url_add_comment=DN::URL('addcomment');
 		DN::Show(get_defined_vars(),'article');
 	}
@@ -81,29 +70,36 @@ class DNController
 	
 	public function do_reg()
 	{
-		$user=UserService::G()->reg(SG::G()->_POST['username'],SG::G()->_POST['password']);
+		try{
+			$user=UserService::G()->reg(SG::G()->_POST['username'],SG::G()->_POST['password']);
+		}catch(DNException $ex){
+			DN::G()->assignViewData('error_info'=>$ex->getMessage());
+			return $this->reg();
+		}
 		SessionService::G()->setCurrentUser($user);
 		DN::ExitRouteTo('');
 	}
 	public function do_login()
 	{
-		$user=UserService::G()->login(SG::G()->_POST['username'],SG::G()->_POST['password']);
+		try{
+			$user=UserService::G()->login(SG::G()->_POST['username'],SG::G()->_POST['password']);
+		}catch(DNException $ex){
+			DN::G()->assignViewData('error_info'=>$ex->getMessage());
+			return $this->login();
+		}
 		SessionService::G()->setCurrentUser($user);
-		
 		DN::ExitRouteTo('');
 	}
 	public function do_addcomment()
 	{
 		$user=SessionService::G()->getCurrentUser();
 		UserService::G()->addComment($user['id'],SG::G()->_POST['article_id'],SG::G()->_POST['content']);
-		
 		DN::ExitRouteTo('');
 	}
 	public function do_delcomment()
 	{
 		$user=SessionService::G()->getCurrentUser();
 		UserService::G()->deleteCommentByUser($user['id'],SG::G()->_POST['id']);
-		
 		DN::ExitRouteTo('');
 	}
 }
