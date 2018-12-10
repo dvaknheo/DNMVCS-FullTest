05e22ad1931098ac7a79c365fdf22e901df0f2ff
dvakheo <dvaknheo@gmail.com>
2018-12-10 21:06:05 +0800

    修改  composer.json ,修改 swoole 启动文件

--- bin/start_server.php
+++ bin/start_server.php
@@ -1,34 +1,39 @@
 <?php
-$IN_COMPOSER=false;
+$file=realpath(__DIR__.'/../../DNMVCS/DNMVCS.php');
+if(is_file($file)){
+	require $file;
+}else{
+////[[[[
+$IN_COMPOSER=true;
 
 $project_root=realpath(__DIR__.'/..');
 
 if($IN_COMPOSER){
 	require($project_root.'/vendor/autoload.php');
 }else{
-	require($project_root.'/../DNMVCS/DNMVCS.php');
+	require($project_root.'/../DNMVCS.php');
 }
-
-$server=null;
-//$server=new swoole_http_server('0.0.0.0', 9528);
+////]]]]
+}
+////[[[[
 $path=realpath(__DIR__.'/../').'/';
-$swoole_options=[
-	//'document_root'=>$path.'static',
-    //'enable_static_handler' => true,
-	//'worker_num'=>1,
-];
-//$server->set($swoole_options);
 $server_options=[
 	'port'=>9528,
-	'swoole_server'=>null,
-	'swoole_server_options'=>$swoole_options,
-	//'http_handler_root'=>$path.'www/',
-	'http_handler_file'=>$path.'www/index.php',
-
+	//'http_handler_basepath'=>$path,
 ];
+
 $dn_options=[
 	'path'=>$path,
 ];
-\DNMVCS\DNMVCS::RunAsServer($server_options,$dn_options,$server);
 
-return;
+////]]]]
+
+$setting=[];
+$setting_file=$path.'config/setting.php';
+if(is_file($setting_file)){
+	$setting=include($setting_file);
+}
+$server_options=array_replace_recursive($server_options,$setting['server_options']??[]);
+
+$server=null;
+\DNMVCS\DNMVCS::RunAsServer($server_options,$dn_options,$server);
