dc3ed1d3255688df913658aa4623aa99e3e57a72
dvakheo <dvaknheo@gmail.com>
2019-05-09 16:39:36 +0800

    修复分页

--- public/u/app/Base/ControllerHelper.php
+++ public/u/app/Base/ControllerHelper.php
@@ -5,9 +5,5 @@ use DNMVCS\Base\ControllerHelper as Helper;
 
 class ControllerHelper extends Helper
 {
-    //
-    public static function Pager()
-    {
-        return \DNMVCS\Base\Pager::G();
-    }
+
 }
