ffeafabd7f5d5b1ba0bb6878710a180964818a3b
dvakheo <dvaknheo@gmail.com>
2019-09-16 15:52:02 +0800

    随版本更新而更新

--- public/u/app/Base/App.php
+++ public/u/app/Base/App.php
@@ -4,12 +4,8 @@ use \DNMVCS\DNMVCS as DN;
 
 class App extends \DNMVCS\DNMVCS
 {
-	public function init($options=[],$context=null)
+	public function onInit()
 	{
-		//$options['ext']['use_strict_db_manager']=true;
-		//$options['ext']['use_ext_db']=true;
-		parent::init($options,$context);
-        
 		$this->assignRewrite([
 			'~article/(\d+)/?(\d+)?'=>'article?id=$1&page=$2',
 		]);
--- public/u/app/Base/AppEx.php
+++ public/u/app/Base/AppEx.php
@@ -6,15 +6,13 @@ class AppEx extends \DNMVCS\DNMVCS
 {
 	public $NO_SETTING=false;
 	public $is_stop=false;
-	public function init($options=[],$context=null)
+	public function onInit()
 	{
 		//$options['ext']['use_strict_db_manager']=true;
-		$options['ext']['use_ext_db']=true;
-		parent::init($options);
 		DN::G()->assignRewrite([
 			'~article/(\d+)/?(\d+)?'=>'article?id=$1&page=$2',
 		]);
-		
+
 		DN::G()->assignRoute([
 			'~abc(\d*)'=>function($x){var_dump("work",$x);},
 		]);
